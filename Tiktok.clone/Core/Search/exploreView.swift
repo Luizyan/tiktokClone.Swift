26810728abe33d5ad64b5f2210ffea06d84cc22e
Luiz Yan <luizyan68@gmail.com>
2025-11-01 10:57:29 -0300

    funcionalidades como barra de rolagem e a area de explorer

---
+++ Tiktok.clone/Core/Search/exploreView.swift
@@ -0,0 +1,18 @@
+//
+//  exploreView.swift
+//  Tiktok.clone
+//
+//  Created by Luiz Yan on 29/10/25.
+//
+
+import SwiftUI
+
+struct exploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    exploreView()
+}
---
+++ Tiktok.clone/Core/Search/userCell.swift
@@ -0,0 +1,18 @@
+//
+//  userCell.swift
+//  Tiktok.clone
+//
+//  Created by Luiz Yan on 01/11/25.
+//
+
+import SwiftUI
+
+struct userCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    userCell()
+}
